385acef4c555219c92f52505c073d3ffa4288a75
AndresTrade <130797623+AndresTrade@users.noreply.github.com>
2023-07-12 19:32:41 -0500

    Control de cafeteria
    Primer presentacion de la maquetacion. Pantalla de login y pantanlla de inicio

---
+++ script.js
@@ -0,0 +1,9 @@
+function gotoIngreso(link){
+    console.log(link.value);
+    location.href = link.value;
+}
+
+function gotoSalida(link){
+    console.log(link.value);
+    location.href = link.value;
+}
